b076bcfa583dedba0ee7f2ccf6cd6d21c03e62ec
hlageek <radim.hladik@fulbrightmail.org>
2021-10-31 00:50:16 +0200

    guide update

--- R/mod_guide.R
+++ R/mod_guide.R
@@ -35,10 +35,6 @@ mod_guide_server <- function(id) {
         
         h2("Instructions"),
         
-      
-        br(),
-        tags$li("After you have filled your name in the", tags$b( "researcher's details"), "section a preview of your report will appear here."),
-        
         tags$li("Your name will be used to search the ASEP repository to provide selectable options in sections", tags$b( "I. & II.")),
         
         tags$li("Use the menu on the left to navigate in the application and continue to fill all relevant", tags$b("report sections.")),
@@ -51,16 +47,20 @@ mod_guide_server <- function(id) {
         
         tags$li(HTML("<i class='fa fa-warning'></i>"), "To avoid data loss, do not refresh the browser while using the app!", style = "color:red"),
         
+        h3("Navigation"),
         
-        p("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis. Etiam posuere lacus quis dolor. Phasellus et lorem id felis nonummy placerat. Phasellus rhoncus. In convallis. Aenean id metus id velit ullamcorper pulvinar. Proin in tellus sit amet nibh dignissim sagittis. Vivamus luctus egestas leo. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean placerat. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Morbi leo mi, nonummy eget tristique non, rhoncus non leo."),
+        img(src="https://owncloud.cesnet.cz/index.php/s/sEf5XIUg7d0jNKq/download",
+            width="100%"),
         
-        h3("Test"),
-        
-        p("  Etiam posuere lacus quis dolor. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Sed ac dolor sit amet purus malesuada congue. Aliquam erat volutpat. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Curabitur bibendum justo non orci. Pellentesque sapien. Aliquam erat volutpat. Etiam posuere lacus quis dolor. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Aliquam erat volutpat. Maecenas aliquet accumsan leo. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi scelerisque luctus velit. Praesent id justo in neque elementum ultrices."),
-        
-        p("  Etiam posuere lacus quis dolor. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Sed ac dolor sit amet purus malesuada congue. Aliquam erat volutpat. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Curabitur bibendum justo non orci. Pellentesque sapien. Aliquam erat volutpat. Etiam posuere lacus quis dolor. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Aliquam erat volutpat. Maecenas aliquet accumsan leo. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi scelerisque luctus velit. Praesent id justo in neque elementum ultrices."),
-        
-        p("  Etiam posuere lacus quis dolor. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Sed ac dolor sit amet purus malesuada congue. Aliquam erat volutpat. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Curabitur bibendum justo non orci. Pellentesque sapien. Aliquam erat volutpat. Etiam posuere lacus quis dolor. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Aliquam erat volutpat. Maecenas aliquet accumsan leo. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi scelerisque luctus velit. Praesent id justo in neque elementum ultrices.")
+        tags$ol(
+          tags$li(tags$b("Vertical navigation"), "for the application and switching between", tags$b("Preview"), "and", tags$b("Report sections"), "."),
+          
+          tags$li(tags$b("Horizontal navigation"), "for the inside of the report sections. (Only appears in subdivided sections.)"),
+          
+          tags$li(tags$b("Section input"), "panel for adding individual report items."),
+          
+          tags$li(tags$b("Section preview"), "to display previous inputs. Selected inputs can be deleted here.")
+        )
         
         
           
