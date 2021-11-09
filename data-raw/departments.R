d5e39a03112b49b833f56454a1499fabda91be95
hlageek <radim.hladik@fulbrightmail.org>
2021-11-09 13:47:55 +0100

    change emails

--- data-raw/departments.R
+++ data-raw/departments.R
@@ -3,19 +3,19 @@
 
 departments <- tibble::tribble(
     ~department_name, ~department_abbrev, ~head_email,
-        "Oddělení analytické filosofie", "OAF", "hladik@flu.cas.cz",
-        "Oddělení současné kontinentální filosofie", "OSKF", "hladik@flu.cas.cz",
-        "Oddělení logiky","OL", "hladik@flu.cas.cz",
-        "Oddělení pro studium antického a středověkého myšlení","OSASM", "hladik@flu.cas.cz",
-        "Oddělení pro komeniologii a intelektuální dějiny raného novověku", "OKIDRN", "hladik@flu.cas.cz",
-        "Oddělení pro studium novověké racionality", "OSNR", "hladik@flu.cas.cz",
-        "Oddělení pro studium moderní české filosofie", "OSMCF","hladik@flu.cas.cz",
-        "Centrum pro teoretická studia", "CTS","hladik@flu.cas.cz",
-        "Centrum globálních studií", "CGS","hladik@flu.cas.cz",
-        "Kabinet pro studium vědy, techniky a společnosti", "KVTS", "hladik@flu.cas.cz",
-        "Kabinet pro klasická studia", "KKS", "hladik@flu.cas.cz",
-        "Centrum medievistických studií", "CMS", "hladik@flu.cas.cz",
-        "Archiv Jana Patočky", "AJP","hladik@flu.cas.cz"
+        "Oddělení analytické filosofie", "OAF", "novotna@flu.cas.cz",
+        "Oddělení současné kontinentální filosofie", "OSKF", "novotna@flu.cas.cz",
+        "Oddělení logiky","OL", "novotna@flu.cas.cz",
+        "Oddělení pro studium antického a středověkého myšlení","OSASM", "novotna@flu.cas.cz",
+        "Oddělení pro komeniologii a intelektuální dějiny raného novověku", "OKIDRN", "novotna@flu.cas.cz",
+        "Oddělení pro studium novověké racionality", "OSNR", "novotna@flu.cas.cz",
+        "Oddělení pro studium moderní české filosofie", "OSMCF","novotna@flu.cas.cz",
+        "Centrum pro teoretická studia", "CTS","novotna@flu.cas.cz",
+        "Centrum globálních studií", "CGS","novotna@flu.cas.cz",
+        "Kabinet pro studium vědy, techniky a společnosti", "KVTS", "novotna@flu.cas.cz",
+        "Kabinet pro klasická studia", "KKS", "novotna@flu.cas.cz",
+        "Centrum medievistických studií", "CMS", "novotna@flu.cas.cz",
+        "Archiv Jana Patočky", "AJP","novotna@flu.cas.cz"
     )
 
 usethis::use_data(departments, overwrite = TRUE)
